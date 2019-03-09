057d93ade33e9f59b575a3b2fbce438c80ffd94e
Clifford <thomasmclifford96@gmail.com>
2019-03-09 02:50:29 -0600

    - Add support for reconnecting to master and spinning up new producers

--- java/consumer/Consumer.java
+++ java/consumer/Consumer.java
@@ -1,6 +1,9 @@
 package consumer;
 
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
 
 
 public class Consumer {
@@ -44,16 +47,43 @@ public class Consumer {
         return (port >= 0 && port <= 65535);
     }
 
+    private static void spawnMinions(String[] producerAddresses) {
+        for (String address: producerAddresses) {
+            if (address.trim().isEmpty()) {
+                break;
+            }
+
+            System.out.println("Received producer address from master: " + address);
+
+            // format is ip:port
+            int portSeparator = address.indexOf(":");
+            String producerHostName = address.substring(0, portSeparator);
+            int producerPort = Integer.parseInt(address.substring(portSeparator + 1));
+
+            try {
+                // Start minion thread for consumer
+                EasySocket minion = new EasySocket(producerHostName, producerPort);
+                System.out.println("Minion connected to producer with address: " + address);
+                new Thread(new TcpMinion(minion)).start();
+            } catch (IOException ex) {
+                System.err.println("Minion failed to connect to producer with address: " + address);
+            }
+        }
+    }
+
     private static void runReceiverService(final String HOSTNAME,
                                            final int SERVER_PORT) {
         try {
-            // Try to connect to server..
+            // Try to setup listener for master..
+            TcpMasterService tcpMasterService = new TcpMasterService();
+            new Thread(tcpMasterService).start();
+
+            // Try to connect to master..
             EasySocket master = new EasySocket(HOSTNAME, SERVER_PORT);
             System.out.println("Connected to host..\n");
 
             // Send master notice of availability
-            // todo port number needs to be dynamic
-            master.writeString("consumer\n7070");
+            master.writeString("consumer\n" + tcpMasterService.getPort());
 
             // Get list of producers
             String[] listOfAddressesFromMaster = master.readString().split("\\r?\\n");
@@ -62,34 +92,17 @@ public class Consumer {
             master.writeString("consumer says OK");
             master.closeStreams();
 
-            // todo Start up 'sever' part of consumer
-
             // Create a minion for each producer address
-            for (String address: listOfAddressesFromMaster) {
-                if (address.trim().isEmpty()) {
-                    break;
-                }
-
-                System.out.println("Received producer address from master: " + address);
-
-                // format is ip:port
-                int portSeparator = address.indexOf(":");
-                String producerHostName = address.substring(0, portSeparator);
-                int producerPort = Integer.parseInt(address.substring(portSeparator + 1));
-
-                // Start minion thread for consumer
-                EasySocket minion = new EasySocket(producerHostName, producerPort);
-                System.out.println("Minion is connected has connected to producer with address: " + address);
-                new Thread(new TcpMinion(minion)).start();
-            }
+            spawnMinions(listOfAddressesFromMaster);
 
         } catch (IOException ex) {
-            System.err.println("Failed to connect to host.");
+            System.err.println("Failed setup initial connection to master.");
             System.exit(1);
         }
+
     }
 
-
+    // todo double check is static ok here?
     private static class TcpMinion implements Runnable {
 
         private final EasySocket minion;
@@ -113,9 +126,83 @@ public class Consumer {
             } catch (IOException ex) {
                 ex.printStackTrace();
             } finally {
-                System.out.println("Minion disconnected from host.");
+                System.out.println("Minion disconnected from producer.");
                 minion.closeStreams();
             }
         }
     }
+
+    public static class TcpMasterService implements Runnable {
+
+        private ServerSocket serverSocket;
+
+        @Override
+        public void run() {
+            try {
+                // Setup the welcoming socket.
+                ServerSocket serverSocket = new ServerSocket(0);
+
+                // Notify the service has been setup successfully.
+                System.out.println(
+                        "Starting master server listening service at: " + new Date() + '\n'
+                                + "Master listening service connected to port: "
+                                + serverSocket.getLocalPort());
+
+                // Listen for master; handle master it until it's finished then wait again
+                while (true) {
+                    Socket socket = serverSocket.accept();
+                    EasySocket master = new EasySocket(socket.getInetAddress().getHostAddress(), socket.getPort());
+                    System.out.println("Master detected. Attempting to setup thread.");
+
+                    try {
+                        // Thread for master setup successfully
+                        System.out.println(
+                                "Thread started for master:\n\t"
+                                        + "IP Address: "
+                                        + master.getInetAddress().getHostAddress()
+                                        + " | Name: ");
+
+                        String message= master.readString().trim();
+
+                        if (message.equalsIgnoreCase("SHUTDOWN")) {
+                            // todo make sure all open connections are closed gracefully
+                            // This is a temporary brutal method
+                            System.exit(0);
+                        } else {
+                            // Get list of producers
+                            String[] listOfAddressesFromMaster = master.readString().split("\\r?\\n");
+
+                            // Respond OK
+                            master.writeString("consumer says OK");
+
+                            // Create a minion for each producer address
+                            spawnMinions(listOfAddressesFromMaster);
+                        }
+
+                    } catch (IOException ex) {
+                        // Ignore because it's likely that master is lost.
+                    } finally {
+                        // Losing the master- perform cleanup.
+                        System.out.println(
+                                "Lost master:\n\t"
+                                        + "IP Address: "
+                                        + master.getInetAddress().getHostAddress()
+                                        + "\nPerforming cleanup..");
+                        master.closeStreams();
+                        System.out.println("Cleanup complete.");
+                    }
+                }
+            } catch (IOException ex) {
+                System.err.println("Failed to setup server socket to listen for master.");
+            }
+        }
+
+        int getPort() {
+            if (serverSocket != null) {
+                return serverSocket.getLocalPort();
+            }
+            return 0;
+        }
+    }
+
 }
